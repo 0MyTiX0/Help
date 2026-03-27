ba660ad4c8988a3413312eba6cdd6d0be5fd28e9
0MyTiX0 <nathan.gris74@gmail.com>
2026-03-27 14:43:49 +0100

    feat: arborescence-des-pages

--- src/app/category/subcategory/page.tsx
+++ src/app/category/subcategory/page.tsx
@@ -1,8 +1,8 @@
-export default function Home() {
+export default function Subcategory() {
   return (
     <main className="main">
       <section className="card">
-        <h1>Homepage</h1>
+        <h1>Subcategory</h1>
       </section>
     </main>
   );
